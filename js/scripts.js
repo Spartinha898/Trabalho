6b1ad9dc5585d33da88842033290780dd7a53768
Spartinha898 <magynopartinha@gmail.com>
2026-09-23 21:34:51 -0300

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,11 @@
-/*!
-* Start Bootstrap - Business Casual v7.0.9 (https://startbootstrap.com/theme/business-casual)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-casual/blob/master/LICENSE)
-*/
 window.addEventListener('DOMContentLoaded', function () {
     const horarios = document.querySelectorAll('.list-hours li');
     const diaAtual = new Date().getDay();
 
     if (horarios[diaAtual]) {
         horarios[diaAtual].classList.add('today');
-        horarios[diaAtual].setAttribute('aria-current', 'date');
     }
-    const carrossel = document.querySelector('#carrosselPadaria');
+    const carrossel = document.querySelector('#carrosselPizzaria');
     if (carrossel) {
         new bootstrap.Carousel(carrossel, { interval: false });
     }
